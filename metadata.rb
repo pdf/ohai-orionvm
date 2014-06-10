de33e6f5e73c3e60487effb27406ddcf75932b34
Peter Fern <github@obfusc8.org>
2014-06-10 15:23:27 +1000

    Bump version to 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'github@0xc0dedbad.com'
 license           'MIT'
 description       'A tiny Ohai plugin to populate cloud attributes for OrionVM virtual machines'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.0.1'
+version           '0.0.2'
 
 %w{debian ubuntu}.each do |os|
   supports os
